ef2e496852b0dc3026512bf7887f7e6040088758
yashikadhall08 <yashikadhall84@gmail.com>
2020-11-09 09:58:21 +0530

    Update scheduleBox.js

--- src/scheduleBox.js
+++ src/scheduleBox.js
@@ -3,10 +3,12 @@ import "./style.css"
 
 
 const Schedulebox =(props) =>{
+   let month = props.month.slice(0,3)
+   
     return(
         <div className="box">
            <div className="datebox" >
-              <h3>{props.day} {props.month}</h3> 
+              <h3>{props.day} {month}</h3> 
            </div>
            <div className="contentbox">
               <p style={{fontWeight: "bold"}}>{props.title}</p>
